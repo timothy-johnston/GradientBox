939d5dd318fe240b5de5407ac90e10c5935e2f41
Tim Johnston <tedwardj11@gmail.com>
2019-08-04 00:20:27 -0400

    added and wired up UI view

--- gradientbox/api/views.py
+++ gradientbox/api/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def index(request):
+    return render(request, 'api/index.html', context=None)
+
 def getRandomGradient(request):
+    print ('Hello, world!')
     return HttpResponse("This will be a random gradient.")
+
+# def addGradient()
 
