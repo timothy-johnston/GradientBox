b60d0c594e3dd68eae8a7798db7ab7d932b27ac2
Tim Johnston <tedwardj11@gmail.com>
2019-08-03 23:52:03 -0400

    created Gradient db model

--- gradientbox/api/models.py
+++ gradientbox/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Gradient(models.Model):
+    gradient_css = models.CharField(max_length = 256)
+    gradient_name = models.CharField(max_length = 256)
+    gradient_author = models.CharField(max_length = 256)
+    def __str__(self):
+        return (self.gradient_css) 
+
