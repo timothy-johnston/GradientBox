f3db6ea5365abe4c5a0ae3d44edc63fa75ee1fac
Tim Johnston <tedwardj11@gmail.com>
2019-08-04 13:48:43 -0400

    added randomGradient endpoint returning json

--- gradientbox/api/views.py
+++ gradientbox/api/views.py
@@ -2,18 +2,23 @@ from rest_framework import viewsets
 from django.shortcuts import render
 from django.http import HttpResponse
 from api.models import Gradient
+from django.core import serializers
 from .serializers import GradientSerializer
+import random
 
 def index(request):
     return render(request, 'api/index.html', context=None)
 
 def getRandomGradient(request):
-    return HttpResponse("This will be a random gradient.")
 
-# def addGradient(request):
-#     print ("testing")
-#     print (request.data)
-#     return HttpResponse("it was a test")
+    #Get all gradients from db
+    queryset = Gradient.objects.all()
+
+    #Choose a gradient at random
+    randomGradient = Gradient.objects.filter(pk = random.randint(1, len(queryset)))
+
+    #Serialize and return the randomly selected gradient
+    return HttpResponse(serializers.serialize('json', randomGradient))
 
 class GradientViewSet(viewsets.ModelViewSet):
     queryset = Gradient.objects.all()
