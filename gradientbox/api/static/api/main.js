075cc7f89d78795007716f0df145586628f55220
Tim <tedwardj11@gmail.com>
2019-08-07 18:04:40 +0000

    fixed get csrf bug

--- gradientbox/api/static/api/main.js
+++ gradientbox/api/static/api/main.js
@@ -244,8 +244,8 @@ function getCookie(cookieName) {
 		var cookies = document.cookie.split(';');
 		for (var i = 0; i < cookies.length; i++) {
 			var cookie = jQuery.trim(cookies[i]);
-			if (cookie.substring(0, name.length + 1) === (name + '=')) {
-				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+			if (cookie.substring(0, cookieName.length + 1) === (cookieName + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(cookieName.length + 1));
 				break;
 			}
 		}
